175ac8dd1d414ef6cdbcaed5c68f015e0a4702e1
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-07-04 20:41:57 +0530

    feat: implemented the hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import City from "./components/City"
+import Hero from "./components/Hero"
+
+
 const App = () => {
   return (
     <div>
-      
+      <Hero/>
+      <City/>
     </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,20 @@
 const Hero = () => {
+  
   return (
-    <div className="h-[100vh] flex justify-center items-center bg-cover bg-center" style={{
-        backgroundImage: 'url("https://www.spacex.com/static/images/mars/2025_Update/mars_hero_d.jpg")',
-      }}>
-        <div className="flex flex-col items-center justify-center mb-30 text-white">
-          <h1 className="text-[88px] font-bold ">MARS & BEYOND</h1>
+    <div
+      className="h-[100vh] w-full flex justify-center items-center bg-cover bg-center overflow-hidden"
+      id="image"
+      style={{
+        backgroundImage:
+          'url("https://www.spacex.com/static/images/mars/2025_Update/mars_hero_d.jpg")',
+      }}
+    >
+      <div className="flex flex-col items-center justify-center mb-30 text-white">
+        <h1 className="text-[88px] font-bold ">MARS & BEYOND</h1>
         <p>THE ROAD TO MAKING HUMANITY MULTIPLANETARY</p>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
