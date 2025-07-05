4ca343892fffe960f2e2c3ffd1cdaa92e351194c
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-07-05 12:33:07 +0530

    feat: starship video and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import City from "./components/City"
 import Hero from "./components/Hero"
+import Starship from "./components/Starship"
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Hero/>
       <City/>
       <About/>
+      <Starship/>
 
     </div>
   )
--- src/components/Starship.tsx
+++ src/components/Starship.tsx
@@ -1,7 +1,17 @@
+import spaceshipVideo from '../assets/spaceship.mp4';
 const Starship = () => {
   return (
-    <div>Starship</div>
-  )
-}
+    <div className="h-[100vh] w-full relative bg-cover bg-center">
+      <video
+        className="absolute inset-0 w-full h-full object-cover "
+        src={spaceshipVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+    </div>
+  );
+};
 
-export default Starship
+export default Starship;
